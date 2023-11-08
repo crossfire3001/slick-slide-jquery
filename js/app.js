e382b793342beaee4a9fc03f551fa7b84b14e494
crossfire3001 <obiwan2035@mail.ru>
2023-11-08 12:33:48 +0500

    2nd slider feedback slider progress

--- js/app.js
+++ js/app.js
@@ -28,5 +28,36 @@ $(".carousel").slick({
   ],
 });
 
+$(".feedback__carousel").slick({
+  rows: 1,
+  slidesPerRow: 4,
+  prevArrow: '<button type="button" class="slick-prev"><img src="images/fork.png" alt="fork"></button>', // Custom HTML for the previous button
+  nextArrow: '<button type="button" class="slick-next"><img src="images/knife.png" alt="knife"></button>', // Custom HTML for the next button
+  responsive: [
+    {
+      breakpoint: 576,
+      settings: {
+        slidesPerRow: 2,
+        rows: 2,
+      },
+    },
+    {
+      breakpoint: 768,
+      settings: {
+        slidesPerRow: 2,
+        rows: 1,
+      },
+    },
+    {
+      breakpoint: 992,
+      settings: {
+        slidesPerRow: 2,
+        rows: 1
+      },
+    },
+  ],
+});
+
+
 
 
