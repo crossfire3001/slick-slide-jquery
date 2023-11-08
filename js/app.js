a7cdf679b2500d776ae6115aaa03f44a050087e3
crossfire3001 <obiwan2035@mail.ru>
2023-11-08 16:26:56 +0500

    slider working properly

--- js/app.js
+++ js/app.js
@@ -30,14 +30,14 @@ $(".carousel").slick({
 
 $(".feedback__carousel").slick({
   rows: 1,
-  slidesPerRow: 4,
+  slidesPerRow: 3,
   prevArrow: '<button type="button" class="slick-prev"><img src="images/fork.png" alt="fork"></button>', // Custom HTML for the previous button
   nextArrow: '<button type="button" class="slick-next"><img src="images/knife.png" alt="knife"></button>', // Custom HTML for the next button
   responsive: [
     {
       breakpoint: 576,
       settings: {
-        slidesPerRow: 2,
+        slidesPerRow: 1,
         rows: 2,
       },
     },
